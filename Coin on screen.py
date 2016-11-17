908c52c8baf795fb3f98706568d91249b38502bd
dragoonmon <jandug3@uni.coventry.ac.uk>
2016-11-17 13:19:11 +0000

    Add files via upload

--- Coin on screen.py
+++ Coin on screen.py
@@ -1,17 +1,36 @@
 from tkinter import *
 
-root = Tk() 
+def Collected(event=None):
+    global CoinCounter
+    CoinCounter += 1
+    Text.config(text="Coin Counter = " + str(CoinCounter))
+
+    
+root = Tk()
+root.geometry("500x500")
+root.title("Coin module")
+
+global CoinCounter
+CoinCounter = 0
 
 coin = PhotoImage(file="/media/removable/SD Card/Python - PUT INTO MEM STICK/TK/Coin.gif", 
-                  format = "gif -index 0"  ) # Opening image of coin
-w1 = Label(root, image=coin).pack(side = "right")
+                  format = "gif -index 0" ) # Opening image of coin
+
+w1 = Label(root, image=coin).pack(side = "left")
+
+Text = Label(root,
+            text="Coin Counter = 0" ,
+            font = ('Times'))# Showing the counter
+Text.pack(side = "right")
+
+CollectCoin = Button(root, text="Collect", command=Collected).pack(side = "left")
 
 
         
 
-Text = Label(root,
-            text="Coin Counter = 0",
-            font = ('Times')).pack(side = "left") # Showing the counter
+
+
+
 
 
 
