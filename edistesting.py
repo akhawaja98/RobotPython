baa0ae9b8b972afdc62b97e97f16902b39d860c0
Edvinas <ka.edvinas@gmail.com>
2016-11-22 16:53:33 +0000

    Add files via upload

---
+++ edistesting.py
@@ -0,0 +1,7 @@
+landm1 = w.coords(Landmark)
+            if landm1[0] == Pos[4]  or landm1[4] == Pos[0]:
+                XSpeed = -XSpeed
+            if landm1[1] == Pos[3] or landm1[3] == Pos[1]:
+                YSpeed = -YSpeed
+
+
