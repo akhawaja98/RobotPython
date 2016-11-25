e5eecf289474f86d35c13b14c55ff926155640e2
khawaja5 <khawaja5@uni.coventry.ac.uk>
2016-11-25 16:54:14 +0000

    Updated Lives
    Now using health percentage instead of lives

--- lives.py
+++ lives.py
@@ -1,24 +1,22 @@
 global startlives
-startlives = 4
+startlives = 101
 
 
 def PrintLives():    
     global startlives 
-    startliveslabel = Label(root, text= "You Have 3 Lives, Good Luck", font="Times")
+    startliveslabel = Label(root, text= "Health 100%", font="Times")
     startliveslabel.place(x=100, y=30) #positioning the lives label in a suitable area of the canvas
 
     
     startlives -= 1
-    startliveslabel.config(text="You Have " + str(startlives) + " Lives") #.config allows us to change the labels text
+    startliveslabel.config(text="Health = " + str(startlives) + "%") #.config allows us to change the labels text
 
     if startlives <= 0:
-        startliveslabel.config(text="GAME OVER - No Lives Left")
-        
         root.destroy()
 
 
     if startlives == 1:
-        startliveslabel.config(text="You Have 1 Life Left")
+        startliveslabel.config(text="1% Health Left Be Careful")
 
 loselifebtn = Button(root, text="Lost Life", command=PrintLives)  #executes the PrintLives function above
 loselifebtn.pack()
